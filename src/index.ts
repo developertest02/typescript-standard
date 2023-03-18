578bbb737f1ea35ba18b3915f6b030c7fe437b47
developertest_02 <developertest02@gmail.com>
2023-03-18 09:15:04 -0400

    running with parcel

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 // Import stylesheets
 import "./style.css";
+import { run } from "./whiteboards/whiteboard";
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById("app");
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+run();
