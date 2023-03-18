a6ab8d6b25af458c78c6a83e89103299588b3a8c
developertest02 <41734220+developertest02@users.noreply.github.com>
2023-03-18 07:46:39 -0400

    1 file modified, 2 added

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 // Import stylesheets
 import './style.css';
+import { runWhitebaord } from './whiteboard';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-console.log('Derp');
+runWhitebaord();
---
+++ whiteboard/index.ts
@@ -0,0 +1,3 @@
+export function runWhitebaord(): void {
+  console.log('WHat?');
+}
